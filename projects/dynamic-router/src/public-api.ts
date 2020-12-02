d517973489c6919911ac0f897f3b7b4d95efad39
Nicola Bello <nicola.bello.3@gmail.com>
2020-12-02 22:30:13 +0000

    added more exports to dynamic-router

--- projects/dynamic-router/src/public-api.ts
+++ projects/dynamic-router/src/public-api.ts
@@ -4,3 +4,6 @@
 
 export * from './lib/dynamic-router.module';
 export * from './lib/helpers/dynamic-route';
+export * from './lib/services/dynamic-router.service';
+export * from './lib/models/navigation-direction';
+export * from './lib/helpers/can-component-deactivate';
