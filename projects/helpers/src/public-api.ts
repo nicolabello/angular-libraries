ffb5fd1f7511e396412ab2e0e9658459796a93e7
Nicola Bello <nicola.bello.3@gmail.com>
2021-10-09 22:09:36 +0100

    updated exports

--- projects/helpers/src/public-api.ts
+++ projects/helpers/src/public-api.ts
@@ -2,15 +2,16 @@
  * Public API Surface of helpers
  */
 
-export * from './lib/base64';
 export * from './lib/forms/common-value-accessor';
-export * from './lib/device';
-export * from './lib/dom';
+export * from './lib/ngrx/subscribe/subscribe.directive';
+export * from './lib/ngrx/subscribe/subscribe.module';
 export * from './lib/ngrx/from-intersection-observer';
 export * from './lib/ngrx/from-mutation-observer';
 export * from './lib/ngrx/from-resize-observer';
 export * from './lib/ngrx/subscriptions-bucket';
-export * from './lib/ngrx/subscribe/subscribe.module';
+export * from './lib/base64';
+export * from './lib/device';
+export * from './lib/dom';
 export * from './lib/normalize-common-js-import';
 export * from './lib/regexp';
 export * from './lib/typings';
