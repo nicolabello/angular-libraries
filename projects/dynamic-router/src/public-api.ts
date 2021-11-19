38f54a7cc9ed3147283cc0e802ed3a17073b7dce
Nicola Bello <nicola.bello.3@gmail.com>
2021-11-19 11:43:51 +0000

    fix for dynamic router exports

--- projects/dynamic-router/src/public-api.ts
+++ projects/dynamic-router/src/public-api.ts
@@ -7,3 +7,7 @@ export * from './lib/helpers/dynamic-route';
 export * from './lib/services/dynamic-router.service';
 export * from './lib/types/navigation-direction';
 export * from './lib/helpers/can-component-deactivate';
+export * from './lib/components/dynamic-router-outlet/dynamic-router-outlet.component';
+export * from './lib/components/dynamic-router-subview/dynamic-router-subview.component';
+export * from './lib/directives/dynamic-router-link.directive';
+export * from './lib/pipes/dynamic-router-link-active.pipe';
